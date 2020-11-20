241b4c1dab8307ef8590b7e9aaac633cc7452af0
曽我匠 <t-soga@oricon.jp>
2020-11-20 20:27:02 +0900

    [add] Hello Ruby
    リファレンスマニュアル -> プログラム・文・式 -> クラス変数のスコープ
    まで読了
    
    サンプルプログラムをhelloworld.rbへ反映

--- helloworld.rb
+++ helloworld.rb
@@ -1,1 +1,74 @@
-puts("Hello, Ruby!!");
+
+# puts("Hello, Ruby!!");
+
+
+# a = 30;
+# b = 40;
+# c = a + b;
+# puts(c);
+
+# #----------class inherited 1----------
+
+# class Foo
+#     @@foo = 1;
+#     puts(@@foo);
+# end
+
+# class Bar < Foo
+#     puts @@foo += 1;
+# end
+
+# class Baz < Bar
+#     puts @@foo += 1;
+# end
+
+# #----------module include----------
+
+# module Foo
+#     @@foo = 1;
+#     puts @@foo;
+# end
+
+# class Bar
+#     include Foo;
+#     puts @@foo += 1;
+# end
+
+# class Baz
+#     include Foo;
+#     puts @@foo += 1;
+# end
+
+#----------class inherited 2----------
+
+# class Foo
+# end
+
+# class Bar < Foo
+#     @@v = :bar;
+# end
+
+# class Foo
+#     @@v = :foo;
+# end
+
+# class Bar
+#     puts @@v;
+# end
+
+
+#----------class scope----------
+
+class Foo
+    @@a = :a;
+    
+    class << Foo
+        puts @@a;
+    end
+
+    def Foo.a1
+        puts @@a;
+    end
+end
+
+Foo.a1
